8ffaacf72ebfcdd19496f5154e2dd67a880f6cec
onlyhappiness <onlyhappiness96@gmail.com>
2023-08-06 22:39:33 +0900

    :sparkles: Feat: 플레이어 이름 기억하기

--- scripts/SceneManager.gd
+++ scripts/SceneManager.gd
@@ -4,6 +4,12 @@ extends Control
 func _ready():
 	var new_dialog = Dialogic.start("res://timeline.dtl")
 	add_child(new_dialog)
+#	pass
 
 
+
+
+#func start_novel():
+#	var new_dialog = Dialogic.start("res://timeline.dtl")
+#	add_child(new_dialog)
 
