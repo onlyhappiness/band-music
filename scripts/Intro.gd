eef4314e0ec8dc8c8f8ee05af191006b3a5421c8
onlyhappiness <onlyhappiness96@gmail.com>
2023-08-23 23:47:14 +0900

    :sparkles: Feat: 인트로 스크린 추가

---
+++ scripts/Intro.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+# New game
+func _on_Button_pressed():
+	var new_game_scene = preload("res://screens/NewGame.tscn")
+	get_tree().change_scene("res://screens/NewGame.tscn")
+	pass
+
+# Load
+func _on_Button2_pressed():
+	pass # Replace with function body.
